d62e66e8214be2c5ecc6a74c49d65e03d27add91
Ouail <156536081+OuailBou@users.noreply.github.com>
2025-04-16 16:19:17 +0200

    cambios para merge

--- src/main/java/es/uma/demoservice2025/trabajo_grupo_15_taw/dao/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package es.uma.demoservice2025.trabajo_grupo_15_taw.dao;
-
-import es.uma.demoservice2025.trabajo_grupo_15_taw.entity.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Integer> {
-}
