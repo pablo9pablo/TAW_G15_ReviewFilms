92ec5a7689e9ae261e4961bd8ebad3c6983d10de
Ouail <156536081+OuailBou@users.noreply.github.com>
2025-04-19 19:19:02 +0200

    Merge con todo y ajuste

--- src/main/java/es/uma/demoservice2025/trabajo_grupo_15_taw/dao/MovieRepository.java
+++ src/main/java/es/uma/demoservice2025/trabajo_grupo_15_taw/dao/MovieRepository.java
@@ -4,9 +4,11 @@ import es.uma.demoservice2025.trabajo_grupo_15_taw.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface MovieRepository extends JpaRepository<Movie,Integer> {
     //Busqueda
     @Query("select m from Movie m where m.title like concat('%', :titulo, '%')")
